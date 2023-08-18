dbdf9ae02227cfaf5cc638688b9212127539e805
AlejandroCalizaya <alejandro.calizaya@utec.edu.pe>
2023-08-17 20:13:22 -0500

    Modificacion 1

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello World from feature1!")
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+@app.route('/')
+
+def hello():
+    return '<h1>Hello, feature1!</h1>'
